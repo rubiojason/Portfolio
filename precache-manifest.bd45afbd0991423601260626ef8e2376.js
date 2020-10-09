56fbf407ad7fdb52bcc56296f6e9ff1b92332b8f
Jason Rubio <jason@Denises-iMac.local>
2020-10-08 17:42:57 -0700

    Updates

---
+++ precache-manifest.bd45afbd0991423601260626ef8e2376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e886f784e7bec7be5f1a64ab631c7b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1520a5b0940eebca6468",
+    "url": "/Portfolio/static/css/main.cd384feb.chunk.css"
+  },
+  {
+    "revision": "3eaa7e8a92517655f35c",
+    "url": "/Portfolio/static/js/2.b2978ba8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.b2978ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1520a5b0940eebca6468",
+    "url": "/Portfolio/static/js/main.80f91bc4.chunk.js"
+  },
+  {
+    "revision": "90261a1ab73ac84bd242",
+    "url": "/Portfolio/static/js/runtime-main.1282757a.js"
+  }
+]);
